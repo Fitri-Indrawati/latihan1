7c21a6cc7d8e26ac8dfe399d4ffc8e847813861c
Fitri <fitriindrawati37@gmail.com>
2025-07-10 23:57:07 -0700

    belajar

---
+++ latihan1.py
@@ -0,0 +1,11 @@
+nama = input("Masukkan nama Anda: ")
+print(f"Halo, {nama}! Selamat datang di Python.")
+nama = input("Masukkan nama Anda: ")
+umur = int(input("Masukkan umur Anda: "))
+
+if umur >= 18:
+    print(f"Halo, {nama}! Anda sudah dewasa.")
+else:
+    print(f"Halo, {nama}! Anda masih di bawah umur.")
+
+print("Terima kasih telah mencoba program logika if.")
